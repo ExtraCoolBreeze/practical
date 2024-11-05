7e10238d9c1edebfae32b3541d94ebd6de579300
“ExtraCoolBreeze” <craigmcmillan@live.co.uk>
2024-11-05 18:55:13 +0000

    Utilising visual studio 2022, additions have been made to the java that should now check for valid inputs and also added a try catch for formatting

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -1,25 +1,34 @@
-import java.util.Scanner;
-
 class Dec2Hex
 {
-public static int Arg1;
-    public static void main(String args[])    {
-        Arg1 = Integer.parseInt(args[0]);
-        char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-        int rem, num;
-        num = Arg1;
-        String hexadecimal="";
-        System.out.println("Converting the Decimal Value " + num + " to Hex...");
+	public static int Arg1;
+    	public static void main(String[] args)
+	{
+        	if (args.length == 0)
+		{
+        	    System.out.println("Error: No input argument provided. Please provide an integer to convert.");
+            	    return;
+        	}
+        	try
+		{
+	  		Arg1 = Integer.parseInt(args[0]);
+        	}
+		catch (NumberFormatException e)
+		{
+            		System.out.println("Error: Invalid input. Please provide an integer to convert.");
+	    		return;
+		}
 
-        while(num != 0)
-        {
-            rem=num%16;
-            hexadecimal= ch[rem] + hexadecimal;
-            num= num/16;
-        }
-
-        System.out.println("Hexadecimal representation is: " + hexadecimal);
-	System.out.println("Thank you for using this converter");
-	System.out,println("Update 2 SonarQubeTest");
-    }
+		char ch[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+        	int rem, num;
+        	num = Arg1;
+        	String hexadecimal = "";
+        	System.out.println("Converting the Decimal Value " + num + " to Hex...");
+		while (num != 0)
+		{
+            		rem = num%16;
+            		hexadecimal = ch[rem] + hexadecimal;
+            		num = num/16;
+        	}
+		system.out.println("Hexadecimal representation is: " + hexadecimal")
+	}
 }
