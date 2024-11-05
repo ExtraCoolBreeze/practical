ec5aa775f72aae7c9504158b510ab85e9292918c
“ExtraCoolBreeze” <craigmcmillan@live.co.uk>
2024-11-05 19:57:26 +0000

    added semicolon

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -22,7 +22,7 @@ public class Dec2Hex{
         	int rem;
 		int num;
         	num = getArg1();
-		StringBuilder hexadecimal = new StringBuilder()
+		StringBuilder hexadecimal = new StringBuilder();
         	System.out.println("Converting the Decimal Value " + num + " to Hex...");
 		while (num != 0)
 		{
