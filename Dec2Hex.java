a18b42bdcaf1ec9475ebd998a9685e36f2990f7d
“ExtraCoolBreeze” <craigmcmillan@live.co.uk>
2024-11-05 19:22:04 +0000

    added getter and changed variable to lower case

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -1,6 +1,6 @@
 public class Dec2Hex
 {
-	private static int Arg1;
+	private static int arg1;
 
     	public static void main(String[] args)
 	{
@@ -22,7 +22,7 @@ public class Dec2Hex
 		char ch[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
         	int rem;
 		int num;
-        	num = Arg1;
+        	num = getArg1();
         	String hexadecimal = "";
         	System.out.println("Converting the Decimal Value " + num + " to Hex...");
 		while (num != 0)
