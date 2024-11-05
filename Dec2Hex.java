c27510c57705eba979eee4698f871adf0e4afd73
“ExtraCoolBreeze” <craigmcmillan@live.co.uk>
2024-11-05 19:03:33 +0000

    Corrected error with missing semicolon

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -29,6 +29,6 @@ class Dec2Hex
             		hexadecimal = ch[rem] + hexadecimal;
             		num = num/16;
         	}
-		system.out.println("Hexadecimal representation is: " + hexadecimal)
+		system.out.println("Hexadecimal representation is: " + hexadecimal);
 	}
 }
